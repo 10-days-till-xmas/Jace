f5441202879ccbd6d589738bc094f71eff8bf970
10_days_till_xmas <x10_days_till_xmas@outlook.com>
2025-06-20 00:23:44 +0100

    Migrate BasicInterpreterTests to Xunit

--- Jace.Tests/BasicInterpreterTests.cs
+++ Jace.Tests/BasicInterpreterTests.cs
@@ -1,85 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using Jace.Operations;
 using Jace.Execution;
 using Jace.Tests.Mocks;
+using Xunit;
 
-#if NETFX_CORE
-using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
-#elif __ANDROID__
-using NUnit.Framework;
-using TestClass = NUnit.Framework.TestFixtureAttribute;
-using TestMethod = NUnit.Framework.TestAttribute;
-#else
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-#endif
-
-namespace Jace.Tests
+namespace Jace.Tests;
+public class BasicInterpreterTests
 {
-    [TestClass]
-    public class BasicInterpreterTests
+    [Fact]
+    public void TestBasicInterpreterSubstraction()
     {
-        [TestMethod]
-        public void TestBasicInterpreterSubstraction()
-        {
-            IFunctionRegistry functionRegistry = new MockFunctionRegistry();
-            IConstantRegistry constantRegistry = new MockConstantRegistry();
+        IFunctionRegistry functionRegistry = new MockFunctionRegistry();
+        IConstantRegistry constantRegistry = new MockConstantRegistry();
 
-            IExecutor executor = new Interpreter();
-            double result = executor.Execute(new Subtraction(
+        IExecutor executor = new Interpreter();
+        var result = executor.Execute(new Subtraction(
+            DataType.Integer,
+            new IntegerConstant(6),
+            new IntegerConstant(9)), functionRegistry, constantRegistry);
+
+        Assert.Equal(-3.0, result);
+    }
+
+    [Fact]
+    public void TestBasicInterpreter1()
+    {
+        IFunctionRegistry functionRegistry = new MockFunctionRegistry();
+        IConstantRegistry constantRegistry = new MockConstantRegistry();
+
+        IExecutor executor = new Interpreter();
+        // 6 + (2 * 4)
+        var result = executor.Execute(
+            new Addition(
                 DataType.Integer,
                 new IntegerConstant(6),
-                new IntegerConstant(9)), functionRegistry, constantRegistry);
+                new Multiplication(
+                    DataType.Integer, 
+                    new IntegerConstant(2), 
+                    new IntegerConstant(4))), functionRegistry, constantRegistry);
 
-            Assert.AreEqual(-3.0, result);
-        }
+        Assert.Equal(14.0, result);
+    }
 
-        [TestMethod]
-        public void TestBasicInterpreter1()
+    [Fact]
+    public void TestBasicInterpreterWithVariables()
+    {
+        IFunctionRegistry functionRegistry = new MockFunctionRegistry();
+        IConstantRegistry constantRegistry = new MockConstantRegistry();
+
+        var variables = new Dictionary<string, double>
         {
-            IFunctionRegistry functionRegistry = new MockFunctionRegistry();
-            IConstantRegistry constantRegistry = new MockConstantRegistry();
+            { "var1", 2 },
+            { "age", 4 }
+        };
 
-            IExecutor executor = new Interpreter();
-            // 6 + (2 * 4)
-            double result = executor.Execute(
-                new Addition(
-                    DataType.Integer,
-                    new IntegerConstant(6),
+        IExecutor interpreter = new Interpreter();
+        // var1 + 2 * (3 * age)
+        var result = interpreter.Execute(
+            new Addition(DataType.FloatingPoint,
+                new Variable("var1"),
+                new Multiplication(
+                    DataType.FloatingPoint,
+                    new IntegerConstant(2),
                     new Multiplication(
-                        DataType.Integer, 
-                        new IntegerConstant(2), 
-                        new IntegerConstant(4))), functionRegistry, constantRegistry);
+                        DataType.FloatingPoint, 
+                        new IntegerConstant(3),
+                        new Variable("age")))), functionRegistry, constantRegistry, variables);
 
-            Assert.AreEqual(14.0, result);
-        }
-
-        [TestMethod]
-        public void TestBasicInterpreterWithVariables()
-        {
-            IFunctionRegistry functionRegistry = new MockFunctionRegistry();
-            IConstantRegistry constantRegistry = new MockConstantRegistry();
-
-            Dictionary<string, double> variables = new Dictionary<string, double>();
-            variables.Add("var1", 2);
-            variables.Add("age", 4);
-
-            IExecutor interpreter = new Interpreter();
-            // var1 + 2 * (3 * age)
-            double result = interpreter.Execute(
-                new Addition(DataType.FloatingPoint,
-                    new Variable("var1"),
-                    new Multiplication(
-                        DataType.FloatingPoint,
-                        new IntegerConstant(2),
-                        new Multiplication(
-                            DataType.FloatingPoint, 
-                            new IntegerConstant(3),
-                            new Variable("age")))), functionRegistry, constantRegistry, variables);
-
-            Assert.AreEqual(26.0, result);
-        }
+        Assert.Equal(26.0, result);
     }
 }
