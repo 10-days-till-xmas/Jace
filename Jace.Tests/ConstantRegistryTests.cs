32ce2c76d94c9a945de616f4d3ff10965bf86509
10_days_till_xmas <x10_days_till_xmas@outlook.com>
2025-06-25 18:38:57 +0100

    Migrate ConstantRegistryTests.cs to Xunit

--- Jace.Tests/ConstantRegistryTests.cs
+++ Jace.Tests/ConstantRegistryTests.cs
@@ -1,58 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Jace.Execution;
+using Xunit;
 
-#if NETFX_CORE
-using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
-#elif __ANDROID__
-using NUnit.Framework;
-using TestClass = NUnit.Framework.TestFixtureAttribute;
-using TestMethod = NUnit.Framework.TestAttribute;
-#else
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-#endif
+namespace Jace.Tests;
 
-namespace Jace.Tests
+public class ConstantRegistryTests
 {
-    [TestClass]
-    public class ConstantRegistryTests
+    [Fact]
+    public void TestAddConstant()
     {
-        [TestMethod]
-        public void TestAddConstant()
-        {
-            ConstantRegistry registry = new ConstantRegistry(false);
+        var registry = new ConstantRegistry(false);
             
-            registry.RegisterConstant("test", 42.0);
+        registry.RegisterConstant("test", 42.0);
 
-            ConstantInfo functionInfo = registry.GetConstantInfo("test");
+        var functionInfo = registry.GetConstantInfo("test");
             
-            Assert.IsNotNull(functionInfo);
-            Assert.AreEqual("test", functionInfo.ConstantName);
-            Assert.AreEqual(42.0, functionInfo.Value);
-        }
+        Assert.NotNull(functionInfo);
+        Assert.Equal("test", functionInfo.ConstantName);
+        Assert.Equal(42.0, functionInfo.Value);
+    }
 
-        [TestMethod]
-        public void TestOverwritable()
+    [Fact]
+    public void TestOverwritable()
+    {
+        var registry = new ConstantRegistry(false);
+
+        registry.RegisterConstant("test", 42.0);
+        registry.RegisterConstant("test", 26.3);
+    }
+
+    [Fact]
+    public void TestNotOverwritable()
+    {
+        ConstantRegistry registry = new ConstantRegistry(false);
+
+        registry.RegisterConstant("test", 42.0, false);
+
+        Assert.Throws<Exception>(() =>
         {
-            ConstantRegistry registry = new ConstantRegistry(false);
-
-            registry.RegisterConstant("test", 42.0);
-            registry.RegisterConstant("test", 26.3);
-        }
-
-        [TestMethod]
-        public void TestNotOverwritable()
-        {
-            ConstantRegistry registry = new ConstantRegistry(false);
-
-            registry.RegisterConstant("test", 42.0, false);
-
-            AssertExtensions.ThrowsException<Exception>(() =>
-                {
-                    registry.RegisterConstant("test", 26.3, false);
-                });
-        }
+            registry.RegisterConstant("test", 26.3, false);
+        });
     }
 }
