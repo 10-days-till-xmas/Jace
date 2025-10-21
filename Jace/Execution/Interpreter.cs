29bbd7ed9bd0aa7422ed3982713d497d66f81b17
10_days_till_xmas <x10_days_till_xmas@outlook.com>
2025-10-21 21:48:20 +0100

    refactor(interpreter): simplify execution logic and modernize syntax
    - Refactor `Interpreter` with pattern matching for `Operation` type checks
    - Simplify binary and unary operation handling using modern switch expressions
    - Extract reusable boolean-to-double conversions into helper methods
    - Seal `Interpreter` class and remove redundant constructor for efficiency
    - Replace verbose lambdas with concise inline logic for lowercase conversion

--- Jace/Execution/Interpreter.cs
+++ Jace/Execution/Interpreter.cs
@@ -1,32 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Jace.Operations;
 using Jace.Util;
 
 namespace Jace.Execution;
 
-public class Interpreter : IExecutor
+public sealed class Interpreter(bool caseSensitive) : IExecutor
 {
-    private readonly bool caseSensitive;
-
     public Interpreter(): this(false) { }
 
-    public Interpreter(bool caseSensitive)
-    {
-        this.caseSensitive = caseSensitive;
-    }
     public Func<IDictionary<string, double>, double> BuildFormula(Operation operation,
                                                                   IFunctionRegistry functionRegistry,
                                                                   IConstantRegistry constantRegistry)
     {
         return caseSensitive
                    ? variables => Execute(operation, functionRegistry, constantRegistry, variables)
-                   : variables =>
-                   {
-                       variables = EngineUtil.ConvertVariableNamesToLowerCase(variables);
-                       return Execute(operation, functionRegistry, constantRegistry, variables);
-                   };
+                   : variables => Execute(operation, functionRegistry, constantRegistry, EngineUtil.ConvertVariableNamesToLowerCase(variables));
     }
 
     public double Execute(Operation operation, IFunctionRegistry functionRegistry, IConstantRegistry constantRegistry)
@@ -39,143 +28,60 @@ public class Interpreter : IExecutor
                           IConstantRegistry constantRegistry,
                           IDictionary<string, double> variables)
     {
-        if (operation == null)
-            throw new ArgumentNullException(nameof(operation));
-
-        if (operation.GetType() == typeof(IntegerConstant))
+        switch (operation)
         {
-            var constant = (IntegerConstant)operation;
-            return constant.Value;
+            case null:
+                throw new ArgumentNullException(nameof(operation));
+            case IntegerConstant integerConstant:
+                return integerConstant.Value;
+            case FloatingPointConstant floatingPointConstant:
+                return floatingPointConstant.Value;
+            case Variable variable:
+                return variables.TryGetValue(variable.Name, out var value)
+                           ? value
+                           : throw new VariableNotDefinedException($"The variable \"{variable.Name}\" used is not defined.");
+            case Function function:
+                var functionInfo = functionRegistry.GetFunctionInfo(function.FunctionName);
+
+                var arguments = new double[functionInfo.IsDynamicFunc ? function.Arguments.Count : functionInfo.NumberOfParameters];
+                for (var i = 0; i < arguments.Length; i++)
+                    arguments[i] = Execute(function.Arguments[i], functionRegistry, constantRegistry, variables);
+
+                return Invoke(functionInfo.Function, arguments);
+            case UnaryOperation unaryOperation:
+                var arg = Execute(unaryOperation.Argument, functionRegistry, constantRegistry, variables);
+                return unaryOperation switch
+                {
+                    UnaryMinus => -arg,
+                    _ => throw new ArgumentException($"Unsupported unary operation \"{unaryOperation}\".", nameof(operation))
+                };
+            case BinaryOperation binaryOperation:
+                var arg1 = Execute(binaryOperation.Argument1, functionRegistry, constantRegistry, variables);
+                var arg2 = Execute(binaryOperation.Argument2, functionRegistry, constantRegistry, variables);
+                return binaryOperation switch
+                {
+                    Addition => arg1 + arg2,
+                    Subtraction => arg1 - arg2,
+                    Multiplication => arg1 * arg2,
+                    Division => arg1 / arg2,
+                    Modulo => arg1 % arg2,
+                    Exponentiation => Math.Pow(arg1, arg2),
+                    And => (arg1.AsBool() && arg2.AsBool()).AsDouble(),
+                    Or => (arg1.AsBool() || arg2.AsBool()).AsDouble(),
+                    Equal => arg1.Equals(arg2).AsDouble(),
+                    NotEqual => (!arg1.Equals(arg2)).AsDouble(),
+                    LessThan => (arg1 < arg2).AsDouble(),
+                    LessOrEqualThan => (arg1 <= arg2).AsDouble(),
+                    GreaterThan => (arg1 > arg2).AsDouble(),
+                    GreaterOrEqualThan => (arg1 >= arg2).AsDouble(),
+                    _ => throw new ArgumentException($"Unsupported binary operation \"{binaryOperation}\".", nameof(operation))
+                };
+            default:
+                throw new ArgumentException($"Unsupported operation \"{operation.GetType().FullName}\".", nameof(operation));
         }
-
-        if (operation.GetType() == typeof(FloatingPointConstant))
-        {
-            var constant = (FloatingPointConstant)operation;
-            return constant.Value;
-        }
-
-        if (operation.GetType() == typeof(Variable))
-        {
-            var variable = (Variable)operation;
-
-            return variables.TryGetValue(variable.Name, out var value)
-                       ? value
-                       : throw new VariableNotDefinedException($"The variable \"{variable.Name}\" used is not defined.");
-        }
-
-        if (operation.GetType() == typeof(Multiplication))
-        {
-            var multiplication = (Multiplication)operation;
-            return Execute(multiplication.Argument1, functionRegistry, constantRegistry, variables) * Execute(multiplication.Argument2, functionRegistry, constantRegistry, variables);
-        }
-
-        if (operation.GetType() == typeof(Addition))
-        {
-            var addition = (Addition)operation;
-            return Execute(addition.Argument1, functionRegistry, constantRegistry, variables) + Execute(addition.Argument2, functionRegistry, constantRegistry, variables);
-        }
-
-        if (operation.GetType() == typeof(Subtraction))
-        {
-            var addition = (Subtraction)operation;
-            return Execute(addition.Argument1, functionRegistry, constantRegistry, variables) - Execute(addition.Argument2, functionRegistry, constantRegistry, variables);
-        }
-
-        if (operation.GetType() == typeof(Division))
-        {
-            var division = (Division)operation;
-            return Execute(division.Argument1, functionRegistry, constantRegistry, variables) / Execute(division.Argument2, functionRegistry, constantRegistry, variables);
-        }
-
-        if (operation.GetType() == typeof(Modulo))
-        {
-            var division = (Modulo)operation;
-            return Execute(division.Argument1, functionRegistry, constantRegistry, variables) % Execute(division.Argument2, functionRegistry, constantRegistry, variables);
-        }
-
-        if (operation.GetType() == typeof(Exponentiation))
-        {
-            var exponentiation = (Exponentiation)operation;
-            return Math.Pow(Execute(exponentiation.Argument1, functionRegistry, constantRegistry, variables), Execute(exponentiation.Argument2, functionRegistry, constantRegistry, variables));
-        }
-
-        if (operation.GetType() == typeof(UnaryMinus))
-        {
-            var unaryMinus = (UnaryMinus)operation;
-            return -Execute(unaryMinus.Argument, functionRegistry, constantRegistry, variables);
-        }
-
-        if (operation.GetType() == typeof(And))
-        {
-            var and = (And)operation;
-            var operation1 = Execute(and.Argument1, functionRegistry, constantRegistry, variables) != 0;
-            var operation2 = Execute(and.Argument2, functionRegistry, constantRegistry, variables) != 0;
-
-            return (operation1 && operation2) ? 1.0 : 0.0;
-        }
-
-        if (operation.GetType() == typeof(Or))
-        {
-            var or = (Or)operation;
-            var operation1 = Execute(or.Argument1, functionRegistry, constantRegistry, variables) != 0;
-            var operation2 = Execute(or.Argument2, functionRegistry, constantRegistry, variables) != 0;
-
-            return (operation1 || operation2) ? 1.0 : 0.0;
-        }
-
-        if(operation.GetType() == typeof(LessThan))
-        {
-            var lessThan = (LessThan)operation;
-            return (Execute(lessThan.Argument1, functionRegistry, constantRegistry, variables) < Execute(lessThan.Argument2, functionRegistry, constantRegistry, variables)) ? 1.0 : 0.0;
-        }
-
-        if (operation.GetType() == typeof(LessOrEqualThan))
-        {
-            var lessOrEqualThan = (LessOrEqualThan)operation;
-            return (Execute(lessOrEqualThan.Argument1, functionRegistry, constantRegistry, variables) <= Execute(lessOrEqualThan.Argument2, functionRegistry, constantRegistry, variables)) ? 1.0 : 0.0;
-        }
-
-        if (operation.GetType() == typeof(GreaterThan))
-        {
-            var greaterThan = (GreaterThan)operation;
-            return (Execute(greaterThan.Argument1, functionRegistry, constantRegistry, variables) > Execute(greaterThan.Argument2, functionRegistry, constantRegistry, variables)) ? 1.0 : 0.0;
-        }
-
-        if (operation.GetType() == typeof(GreaterOrEqualThan))
-        {
-            var greaterOrEqualThan = (GreaterOrEqualThan)operation;
-            return (Execute(greaterOrEqualThan.Argument1, functionRegistry, constantRegistry, variables) >= Execute(greaterOrEqualThan.Argument2, functionRegistry, constantRegistry, variables)) ? 1.0 : 0.0;
-        }
-
-        if (operation.GetType() == typeof(Equal))
-        {
-            var equal = (Equal)operation;
-            return (Execute(equal.Argument1, functionRegistry, constantRegistry, variables) == Execute(equal.Argument2, functionRegistry, constantRegistry, variables)) ? 1.0 : 0.0;
-        }
-
-        if (operation.GetType() == typeof(NotEqual))
-        {
-            var notEqual = (NotEqual)operation;
-            return (Execute(notEqual.Argument1, functionRegistry, constantRegistry, variables) != Execute(notEqual.Argument2, functionRegistry, constantRegistry, variables)) ? 1.0 : 0.0;
-        }
-
-        if (operation.GetType() == typeof(Function))
-        {
-            var function = (Function)operation;
-
-            var functionInfo = functionRegistry.GetFunctionInfo(function.FunctionName);
-
-            var arguments = new double[functionInfo.IsDynamicFunc ? function.Arguments.Count : functionInfo.NumberOfParameters];
-            for (var i = 0; i < arguments.Length; i++)
-                arguments[i] = Execute(function.Arguments[i], functionRegistry, constantRegistry, variables);
-
-            return Invoke(functionInfo.Function, arguments);
-        }
-
-        throw new ArgumentException($"Unsupported operation \"{operation.GetType().FullName}\".", nameof(operation));
     }
 
-    private double Invoke(Delegate function, double[] arguments)
+    private static double Invoke(Delegate function, double[] arguments)
     {
         var args = arguments;
         return function switch
@@ -216,7 +122,7 @@ public class Interpreter : IExecutor
             Func<double, double, double, double, double, double, double, double, double, double, double, double, double, double, double, double, double> func16
                 => func16.Invoke(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12], args[13], args[14], args[15]),
             DynamicFunc<double, double> dynamicFunc => dynamicFunc.Invoke(args),
-            _ => (double)function.DynamicInvoke((args.Select(s => (object)s)).ToArray())
+            _ => (double)function.DynamicInvoke(args)!
         };
     }
 }
---
+++ Jace/Util/BoolHelpers.cs
@@ -0,0 +1,8 @@
+﻿namespace Jace.Util;
+
+internal static class BoolHelpers
+{
+    public static bool AsBool(this double value) => value != 0;
+    
+    public static double AsDouble(this bool value) => value ? 1 : 0;
+}
